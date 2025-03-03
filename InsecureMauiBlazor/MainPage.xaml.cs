13bc3202e3918ff25eec6490a241317aff8cdb77
Miles Lundqvist <mileslundqvist@gmail.com>
2025-03-03 11:17:12 +0100

    Add project files.

---
+++ InsecureMauiBlazor/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace InsecureMauiBlazor
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
